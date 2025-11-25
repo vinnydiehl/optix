7fc1023636d094eadce0451847276d5681a09c3b
Vinny Diehl <vinny.diehl@gmail.com>
2025-11-25 03:05:56 -0700

    Implement component rotation

--- app/optix/input.rb
+++ app/optix/input.rb
@@ -1,9 +1,12 @@
 class OptixGame
   def handle_mouse_input
+    handle_lmb
+    handle_scroll
+  end
+
+  def handle_lmb
     if @mouse.key_down.left
-      @component_held = @movable_components.find do |c|
-        Geometry.intersect_circle?(Geometry.rect_to_circle(@mouse.rect), c.hitbox)
-      end
+      @component_held = component_under_mouse
     end
 
     if @component_held
@@ -12,6 +15,21 @@ class OptixGame
       elsif @mouse.key_up.left
         @component_held = nil
       end
+    end
+  end
+
+  # Scroll to change the angle of a component
+  def handle_scroll
+    return unless (d = @mouse.wheel&.y)
+    if (c = @component_held || component_under_mouse)
+      c.angle += d
+    end
+  end
+
+  # Returns the movable component under the mouse, or nil.
+  def component_under_mouse
+    @movable_components.find do |c|
+      Geometry.intersect_circle?(Geometry.rect_to_circle(@mouse.rect), c.hitbox)
     end
   end
 end
